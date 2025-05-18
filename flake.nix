b39753af116e9b74995ba97eecbb44ada04c10f1
Kilian Markl <kilian02.markl@gmail.com>
2025-05-18 13:56:02 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -30,91 +30,84 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+    };
+
     rycee-nur = {
       url = "gitlab:rycee/nur-expressions";
       flake = false;
     };
-
-    # Neovim Plugins
-    neomodern-nvim = {
-      url = "github:cdmill/neomodern.nvim/843698e7e3e6199da3e7c89ab9b3d8807d7be69a";
-      flake = false;
-    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
-    let
-      inherit (self) outputs;
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          (final: prev: {
-            qt6Packages = prev.qt6Packages.overrideScope (_: kprev: {
-              qt6gtk2 = kprev.qt6gtk2.overrideAttrs (_: {
-                version = "0.5-unstable-2025-03-04";
-                src = final.fetchFromGitLab {
-                  domain = "opencode.net";
-                  owner = "trialuser";
-                  repo = "qt6gtk2";
-                  rev = "d7c14bec2c7a3d2a37cde60ec059fc0ed4efee67";
-                  hash = "sha256-6xD0lBiGWC3PXFyM2JW16/sDwicw4kWSCnjnNwUT4PI=";
-                };
-              });
-            });
-          })
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+    inherit (nixpkgs) lib;
+  in {
+    packages.${system} = import ./pkgs {inherit pkgs;};
+
+    formatter.${system} = pkgs.alejandra;
+
+    homeManagerModules = import ./modules/home;
+
+    nixosConfigurations = {
+      albrecht = lib.nixosSystem {
+        specialArgs = {
+          inherit inputs outputs self;
+          host = "albrecht";
+        };
+        modules = [
+          ./hosts/albrecht
+          inputs.chaotic.nixosModules.default
         ];
       };
-      inherit (nixpkgs) lib;
-    in
-    {
-      packages.${system} = import ./pkgs { inherit pkgs; };
 
-      formatter.${system} = pkgs.nixpkgs-fmt;
-
-      homeManagerModules = import ./modules/home;
-
-      nixosConfigurations = {
-        albrecht = lib.nixosSystem {
-          specialArgs = {
-            inherit inputs outputs self;
-            host = "albrecht";
-          };
-          modules = [
-            ./hosts/albrecht
-            inputs.chaotic.nixosModules.default
-          ];
+      loid = lib.nixosSystem {
+        specialArgs = {
+          host = "loid";
+          inherit inputs outputs self;
         };
-
-        loid = lib.nixosSystem {
-          specialArgs = {
-            host = "loid";
-            inherit inputs outputs self;
-          };
-          modules = [
-            ./hosts/loid
-          ];
-        };
-      };
-
-      homeConfigurations = {
-        "kilianm@albrecht" = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          extraSpecialArgs = {
-            host = "albrecht";
-            inherit inputs outputs self;
-          };
-          modules = [ ./home self.homeManagerModules.all ];
-        };
-
-        "kilianm@loid" = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          extraSpecialArgs = {
-            host = "loid";
-            inherit inputs outputs self;
-          };
-          modules = [ ./home self.homeManagerModules.all ];
-        };
+        modules = [
+          ./hosts/loid
+        ];
       };
     };
+
+    homeConfigurations = {
+      "kilianm@albrecht" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = {
+          host = "albrecht";
+          inherit inputs outputs self;
+        };
+        modules = [
+          ./home
+          self.homeManagerModules.all
+          inputs.zen-browser.homeModules.beta
+        ];
+      };
+
+      "kilianm@loid" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = {
+          host = "loid";
+          inherit inputs outputs self;
+        };
+        modules = [
+          ./home
+          self.homeManagerModules.all
+          inputs.zen-browser.homeModules.beta
+        ];
+      };
+    };
+  };
 }
