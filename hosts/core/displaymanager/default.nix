6de47a621c46a4d1952dadf3da04520aaed3b561
Kilian Markl <kilian02.markl@gmail.com>
2025-06-04 12:03:25 +0200

    Update flake

--- hosts/core/displaymanager/default.nix
+++ hosts/core/displaymanager/default.nix
@@ -3,15 +3,16 @@
     sessionPackages = with pkgs; [hyprland];
   };
 
+  services.displayManager.gdm = {
+    enable = true;
+    wayland = true;
+  };
+
   services.xserver = {
     enable = true;
     xkb = {
       layout = "us";
       variant = "";
-    };
-    displayManager.gdm = {
-      enable = true;
-      wayland = true;
     };
   };
 }
